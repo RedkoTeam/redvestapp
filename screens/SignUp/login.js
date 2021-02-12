6656845fdaf554b065d707a0c29644e07bf55541
redkoai <redko@theredko.com>
2021-02-12 11:37:35 -0800

    signup sign in UI

--- screens/SignUp/login.js
+++ screens/SignUp/login.js
@@ -2,7 +2,10 @@ import {useNavigation} from "@react-navigation/native";
 import React, {useEffect} from "react";
 import {Image, ImageBackground, Linking, TouchableOpacity, View,ScrollView} from "react-native";
 import back from "../../assets/images/Invest/step4/back.png"
-import hist from "../../assets/images/signup/SignIn.png"
+import title from "../../assets/images/signup/SignIn.png"
+import emailtxt from "../../assets/images/signup/Email.png"
+import passwordtxt from "../../assets/images/signup/Password.png"
+import forgottxt from "../../assets/images/signup/Forgot.png"
 import bg from "../../assets/images/HomeScreen/bg.png"
 import { Dimensions } from 'react-native';
 import {widthPercentageToDP,heightPercentageToDP,} from '../../util/scaler';
@@ -31,7 +34,20 @@ function Login() {
                           </TouchableOpacity>    
   
                   </View>
-       
+                  <Image source={title} style={{marginTop:'10%', width :widthPercentageToDP('30'), height :heightPercentageToDP('5'), resizeMode:'contain',  marginLeft:widthPercentageToDP(15)}}/>
+                <Image source={emailtxt} style={{marginTop:'10%', width :widthPercentageToDP('12'), height :heightPercentageToDP('5'), resizeMode:'contain',  marginLeft:widthPercentageToDP(15)}}/>
+                <Image source={passwordtxt} style={{marginTop:'10%', width :widthPercentageToDP('20'), height :heightPercentageToDP('5'), resizeMode:'contain',  marginLeft:widthPercentageToDP(15)}}/>
+                <TouchableOpacity onPress={()=>navigation.navigate('HomeScreen')} style={{
+                                      left:widthPercentageToDP(3),
+                                      top:heightPercentageToDP(3),
+                                  }}>
+                <Image source={forgottxt} style={{marginTop:'10%', width :widthPercentageToDP('40'), height :heightPercentageToDP('5'), resizeMode:'contain',  marginLeft:widthPercentageToDP(45)}}/>
+                </TouchableOpacity>  
+                <View>
+                <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+                  <Image source={next} style={{resizeMode:'contain',width:widthPercentageToDP(85), height:heightPercentageToDP(11),left:'8%', marginTop:heightPercentageToDP(11) }}/>
+                </TouchableOpacity>
+                </View>
          
   
   
