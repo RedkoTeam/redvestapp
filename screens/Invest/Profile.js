4b14f2ff85f8d7c47582c3f6a4adb0a454d0dca2
sepsol <SR-75@live.com>
2021-02-21 13:24:22 -0800

    added a sign out button

--- screens/Invest/Profile.js
+++ screens/Invest/Profile.js
@@ -1,33 +1,36 @@
-import {useNavigation} from "@react-navigation/native";
-import React, {useEffect} from "react";
-import {Image, ImageBackground, Linking, TouchableOpacity, View,ScrollView} from "react-native";
-import investbtn from "../../assets/images/HomeScreen/investbtn.png"
-import playbtn from "../../assets/images/HomeScreen/playbtn.png"
-import cont from "../../assets/images/HomeScreen/reddollarcon.png"
-import AIone from "../../assets/images/Invest/step1/AIone.png"
-import AItwo from "../../assets/images/Invest/step1/AItwo.png"
-import MACD from "../../assets/images/Invest/step1/MACD.png"
-import RSI from "../../assets/images/Invest/step1/RSI.png"
-import DUAL from "../../assets/images/Invest/step1/DualMa.png"
-import pickStra from "../../assets/images/profile/TotalBalance.png"
-import settings from "../../assets/images/profile/settings.png"
-import cash from "../../assets/images/profile/Cashbalance.png"
-import as from "../../assets/images/profile/activestrat.png"
-import orders from "../../assets/images/profile/orders.png"
-import hist from "../../assets/images/profile/history.png"
-import Threema from "../../assets/images/Invest/step1/Threema.png" 
-import bg from "../../assets/images/HomeScreen/bg.png"
-import loginbtn from "../../assets/images/HomeScreen/login.png"
-import signupbtn from "../../assets/images/HomeScreen/signup.png"
-import {widthPercentageToDP,heightPercentageToDP,} from '../../util/scaler';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useContext } from 'react';
+import AuthContext from 'redvest/contexts/AuthContext';
+import { Image, ImageBackground, Linking, TouchableOpacity, View, ScrollView } from 'react-native';
+import investbtn from '../../assets/images/HomeScreen/investbtn.png';
+import playbtn from '../../assets/images/HomeScreen/playbtn.png';
+import cont from '../../assets/images/HomeScreen/reddollarcon.png';
+import AIone from '../../assets/images/Invest/step1/AIone.png';
+import AItwo from '../../assets/images/Invest/step1/AItwo.png';
+import MACD from '../../assets/images/Invest/step1/MACD.png';
+import RSI from '../../assets/images/Invest/step1/RSI.png';
+import DUAL from '../../assets/images/Invest/step1/DualMa.png';
+import pickStra from '../../assets/images/profile/TotalBalance.png';
+import settings from '../../assets/images/profile/settings.png';
+import cash from '../../assets/images/profile/Cashbalance.png';
+import as from '../../assets/images/profile/activestrat.png';
+import orders from '../../assets/images/profile/orders.png';
+import hist from '../../assets/images/profile/history.png';
+import Threema from '../../assets/images/Invest/step1/Threema.png';
+import bg from '../../assets/images/HomeScreen/bg.png';
+import loginbtn from '../../assets/images/HomeScreen/login.png';
+import signupbtn from '../../assets/images/HomeScreen/signup.png';
+import { widthPercentageToDP, heightPercentageToDP } from '../../util/scaler';
 import { actuatedNormalize } from '../../util/fontScaler';
-import NavBar_pro from "../Navbar/Navbar_pro.js";
-import {enableScreens} from "react-native-screens";
-import { Dimensions } from "react-native";
+import NavBar_pro from '../Navbar/Navbar_pro.js';
+import { enableScreens } from 'react-native-screens';
+import { Dimensions } from 'react-native';
 //import alpacaApi from '../services/alpaca'
-import {LineChart} from "react-native-chart-kit";
+import { LineChart } from 'react-native-chart-kit';
+import CustomButton from 'redvest/components/CustomButton';
 
-{/*class DashboardScreen extends React.Component {
+{
+  /*class DashboardScreen extends React.Component {
 
   static navigationOptions = {
       title: 'Dashboard'
@@ -74,94 +77,168 @@ import {LineChart} from "react-native-chart-kit";
 
 
 
-*/}
+*/
+}
 
 const chartConfig = {
-  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFrom: '#1E2923',
   backgroundGradientFromOpacity: 0,
-  backgroundGradientTo: "#08130D",
+  backgroundGradientTo: '#08130D',
   backgroundGradientToOpacity: 0.5,
   color: (opacity = 1) => `rgba(118, 159, 35,  ${opacity})`,
   strokeWidth: 2, // optional, default 3
   barPercentage: 0.5,
-  useShadowColorFromDataset: false // optional
+  useShadowColorFromDataset: false, // optional
 };
 
-const screenWidth = Dimensions.get("window").width;
+const screenWidth = Dimensions.get('window').width;
 
-enableScreens(false)
-
+enableScreens(false);
 
 function Profile() {
+  const { singOutAsync } = useContext(AuthContext);
   const navigation = useNavigation();
   const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
     datasets: [
       {
         data: [20, 45, 28, 80, 99, 123],
         color: (opacity = 1) => `rgba(118, 159, 35, ${opacity})`, // optional
-        strokeWidth: 4 // optional
-      }
+        strokeWidth: 4, // optional
+      },
     ],
     //legend: [""] // optional
   };
-  useEffect(()=>{
-      let mounted = true;
-  })
+  useEffect(() => {
+    let mounted = true;
+  });
   return (
-      <View style={{flex: 1}}>  
-      <ImageBackground source={bg} style={{width:widthPercentageToDP(100), height:heightPercentageToDP(100)}}>      
-      <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-between', padding: 25, marginTop: 5,marginBottom:'-15%' }}>
-      <Image source={pickStra} style={{resizeMode:'contain',width:widthPercentageToDP(50), height:heightPercentageToDP(10), marginTop: 20, marginLeft:'10%'}} />
-      <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-between', padding: 25, marginTop: '20%',marginBottom:'10%', right:'80%' }}>
-      <LineChart
-  data={data}
-  width={screenWidth}
-  height={256}
-  verticalLabelRotation={30}
-  chartConfig={chartConfig}
-  bezier
-/>
-</View>
-              <TouchableOpacity onPress={() => navigation.navigate('SubscriptionScreen')}>
-              <Image source={settings} style={{resizeMode: 'contain',width: widthPercentageToDP(7),height: heightPercentageToDP(16)}} />
-              </TouchableOpacity>
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        source={bg}
+        style={{ width: widthPercentageToDP(100), height: heightPercentageToDP(100) }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            width: '100%',
+            justifyContent: 'space-between',
+            padding: 25,
+            marginTop: 5,
+            marginBottom: '-15%',
+          }}
+        >
+          <Image
+            source={pickStra}
+            style={{
+              resizeMode: 'contain',
+              width: widthPercentageToDP(50),
+              height: heightPercentageToDP(10),
+              marginTop: 20,
+              marginLeft: '10%',
+            }}
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              justifyContent: 'space-between',
+              padding: 25,
+              marginTop: '20%',
+              marginBottom: '10%',
+              right: '80%',
+            }}
+          >
+            <LineChart
+              data={data}
+              width={screenWidth}
+              height={256}
+              verticalLabelRotation={30}
+              chartConfig={chartConfig}
+              bezier
+            />
           </View>
+          <TouchableOpacity onPress={() => navigation.navigate('SubscriptionScreen')}>
+            <Image
+              source={settings}
+              style={{
+                resizeMode: 'contain',
+                width: widthPercentageToDP(7),
+                height: heightPercentageToDP(16),
+              }}
+            />
+          </TouchableOpacity>
+        </View>
 
-
-        <ScrollView style={{height:'200%'}}>
-              <View style={{  flex: 0.03, flexDirection: 'column', width: '100%', justifyContent: 'center', padding: 15, marginLeft:'3%' }}>
-              <View>
-                <TouchableOpacity onPress={() => navigation.navigate('stepTwo')}>
-                  <Image source={cash} style={{resizeMode:'contain',width:widthPercentageToDP(40), height:heightPercentageToDP(8)}} />
-                </TouchableOpacity>
-                </View>
-                <View>
-                <TouchableOpacity onPress={() => navigation.navigate('ActiveStrategies')}>
-                  <Image source={as} style={{resizeMode:'contain',width:widthPercentageToDP(85), height:heightPercentageToDP(7)}}/>
-                </TouchableOpacity>
-                </View>
-                <View>
-                <TouchableOpacity onPress={() => navigation.navigate('orders')}>
-                  <Image source={orders} style={{resizeMode:'contain',width:widthPercentageToDP(85), height:heightPercentageToDP(7)}} />
-                </TouchableOpacity>
-                </View>
-                <View>
-                <TouchableOpacity onPress={() => navigation.navigate('history')}>
-                  <Image source={hist} style={{resizeMode:'contain',width:widthPercentageToDP(85), height:heightPercentageToDP(7)}}/>
-                </TouchableOpacity>
-                </View>
-        
-              </View>
-              </ScrollView>
-
-
-              
+        <ScrollView
+          style={{ height: '200%', flex: 1 }}
+          contentContainerStyle={{ alignItems: 'center' }}
+        >
+          <CustomButton primary onPress={singOutAsync} text="Sign Out" />
+          <View
+            style={{
+              flex: 0.03,
+              flexDirection: 'column',
+              width: '100%',
+              justifyContent: 'center',
+              padding: 15,
+              marginLeft: '3%',
+            }}
+          >
+            <View>
+              <TouchableOpacity onPress={() => navigation.navigate('stepTwo')}>
+                <Image
+                  source={cash}
+                  style={{
+                    resizeMode: 'contain',
+                    width: widthPercentageToDP(40),
+                    height: heightPercentageToDP(8),
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity onPress={() => navigation.navigate('ActiveStrategies')}>
+                <Image
+                  source={as}
+                  style={{
+                    resizeMode: 'contain',
+                    width: widthPercentageToDP(85),
+                    height: heightPercentageToDP(7),
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity onPress={() => navigation.navigate('orders')}>
+                <Image
+                  source={orders}
+                  style={{
+                    resizeMode: 'contain',
+                    width: widthPercentageToDP(85),
+                    height: heightPercentageToDP(7),
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity onPress={() => navigation.navigate('history')}>
+                <Image
+                  source={hist}
+                  style={{
+                    resizeMode: 'contain',
+                    width: widthPercentageToDP(85),
+                    height: heightPercentageToDP(7),
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
       </ImageBackground>
-      <NavBar_pro/>
-     </View>
-    
-  )
+      <NavBar_pro />
+    </View>
+  );
 }
 
-export default Profile
+export default Profile;
