6fb6875f0981ef69b6c25bb7921ce59fc7d6b1cd
sepsol <SR-75@live.com>
2021-02-27 19:22:38 -0800

    moved the game screens to their own navigator

--- navigators/GameStackNavigator.jsx
+++ navigators/GameStackNavigator.jsx
@@ -3,7 +3,13 @@ import { headerOptions } from 'redvest/util/styles';
 import BackChevron from 'redvest/components/BackChevron';
 
 import { createStackNavigator } from '@react-navigation/stack';
-import GameScreen from 'redvest/screens/game/GameScreen';
+import stepOne from 'redvest/screens/game/stepOne';
+import stepTwo from 'redvest/screens/game/stepTwo';
+import stepThree from 'redvest/screens/game/stepThree';
+import stepThreea from 'redvest/screens/game/stepThreea';
+import stepFour from 'redvest/screens/game/stepFour';
+import stepFive from 'redvest/screens/game/stepFive';
+import stepFiveA from 'redvest/screens/game/stepFiveA';
 
 const GameStack = createStackNavigator();
 
@@ -11,13 +17,19 @@ function AccountStackNavigator() {
   return (
     <GameStack.Navigator screenOptions={headerOptions}>
       <GameStack.Screen
-        name="Game"
-        component={GameScreen}
+        name="stepOne"
+        component={stepOne}
         options={{
           headerLeft: () => <BackChevron />,
           title: 'Game Screen',
         }}
       />
+      <GameStack.Screen name="stepTwo" component={stepTwo} />
+      <GameStack.Screen name="stepThree" component={stepThree} />
+      <GameStack.Screen name="stepThreea" component={stepThreea} />
+      <GameStack.Screen name="stepFour" component={stepFour} />
+      <GameStack.Screen name="stepFive" component={stepFive} />
+      <GameStack.Screen name="stepFiveA" component={stepFiveA} />
     </GameStack.Navigator>
   );
 }
--- navigators/HomeStackNavigator.jsx
+++ navigators/HomeStackNavigator.jsx
@@ -3,13 +3,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from 'redvest/screens/Home/homescreen';
 import Onboarding from 'redvest/screens/Home/homescreen';
-import stepOne from 'redvest/screens/invest/stepOne';
-import stepTwo from 'redvest/screens/invest/stepTwo';
-import stepThree from 'redvest/screens/invest/stepThree';
-import stepThreea from 'redvest/screens/invest/stepThreea';
-import stepFour from 'redvest/screens/invest/stepFour';
-import stepFive from 'redvest/screens/invest/stepFive';
-import stepFiveA from 'redvest/screens/invest/stepFiveA';
 import main from 'redvest/screens/invest/main';
 import tfInfo from 'redvest/screens/info/tfInfo';
 import tickerInfo from 'redvest/screens/info/tickerInfo';
@@ -27,13 +20,6 @@ function HomeStackNavigator() {
     <HomeStack.Navigator screenOptions={{ headerShown: false }}>
       <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
       <HomeStack.Screen name="Onboarding" component={Onboarding} />
-      <HomeStack.Screen name="stepOne" component={stepOne} />
-      <HomeStack.Screen name="stepTwo" component={stepTwo} />
-      <HomeStack.Screen name="stepThree" component={stepThree} />
-      <HomeStack.Screen name="stepThreea" component={stepThreea} />
-      <HomeStack.Screen name="stepFour" component={stepFour} />
-      <HomeStack.Screen name="stepFive" component={stepFive} />
-      <HomeStack.Screen name="stepFiveA" component={stepFiveA} />
       <HomeStack.Screen name="main" component={main} />
       <HomeStack.Screen name="tfInfo" component={tfInfo} />
       <HomeStack.Screen name="typeInfo" component={typeInfo} />
