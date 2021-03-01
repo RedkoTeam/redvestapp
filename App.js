0a2921ba06a79d3b80d0c436bd783a8136df8f27
redkoai <redko@theredko.com>
2021-03-01 13:03:20 -0800

    asset changes

--- App.js
+++ App.js
@@ -29,14 +29,14 @@ import {
 } from '@env';
 
 const FIREBASE_CONFIG = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  databaseURL: DATABASE_URL,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGE_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID,
+  apiKey: "AIzaSyBbELocxybHPNIvOvKhoHBUZxTlNtOTQuM",
+  authDomain: "redvest-48588.firebaseapp.com",
+  databaseURL: "https://redvest-48588-default-rtdb.firebaseio.com",
+  projectId: "redvest-48588",
+  storageBucket: "redvest-48588.appspot.com",
+  messagingSenderId: "651982342789",
+  appId: "1:651982342789:web:399220ff400e2e030c37ec",
+  measurementId: "G-R3FF3VQ1JE",
 };
 
 !firebase.apps.length ? firebase.initializeApp(FIREBASE_CONFIG) : firebase.app();
