4d0bbdc1720f6645f8809868dcda3255f33021f0
sepsol <SR-75@live.com>
2021-03-02 14:43:08 -0800

    renamed InvestScreen and added headers for info screens

--- navigators/InvestStackNavigator.jsx
+++ navigators/InvestStackNavigator.jsx
@@ -3,7 +3,7 @@ import { headerOptions } from 'redvest/util/styles';
 import BackChevron from 'redvest/components/BackChevron';
 
 import { createStackNavigator } from '@react-navigation/stack';
-import Invest from 'redvest/screens/invest/Invest';
+import InvestScreen from 'redvest/screens/invest/InvestScreen';
 import tickerInfo from 'redvest/screens/info/tickerInfo';
 import typeInfo from 'redvest/screens/info/typeInfo';
 import tfInfo from 'redvest/screens/info/tfInfo';
@@ -14,11 +14,39 @@ const InvestStack = createStackNavigator();
 function InvestStackNavigator() {
   return (
     <InvestStack.Navigator screenOptions={headerOptions}>
-      <InvestStack.Screen name="Invest" component={Invest} options={{ headerShown: false }} />
-      <InvestStack.Screen name="tickerInfo" component={tickerInfo} />
-      <InvestStack.Screen name="typeInfo" component={typeInfo} />
-      <InvestStack.Screen name="tfInfo" component={tfInfo} />
-      <InvestStack.Screen name="slInfo" component={slInfo} />
+      <InvestStack.Screen name="Invest" component={InvestScreen} options={{ headerShown: false }} />
+      <InvestStack.Screen
+        name="tickerInfo"
+        component={tickerInfo}
+        options={{
+          headerLeft: () => <BackChevron />,
+          title: 'Stock ticker info',
+        }}
+      />
+      <InvestStack.Screen
+        name="typeInfo"
+        component={typeInfo}
+        options={{
+          headerLeft: () => <BackChevron />,
+          title: 'Order type info',
+        }}
+      />
+      <InvestStack.Screen
+        name="tfInfo"
+        component={tfInfo}
+        options={{
+          headerLeft: () => <BackChevron />,
+          title: 'Time in force info',
+        }}
+      />
+      <InvestStack.Screen
+        name="slInfo"
+        component={slInfo}
+        options={{
+          headerLeft: () => <BackChevron />,
+          title: 'Stop loss info',
+        }}
+      />
     </InvestStack.Navigator>
   );
 }
