433b3c0eb1d08dd58ddce599cbc8152760a6eedc
sepsol <SR-75@live.com>
2021-03-01 17:52:06 -0800

    moved screens related to invest tab to their own navigator

--- navigators/InvestStackNavigator.jsx
+++ navigators/InvestStackNavigator.jsx
@@ -4,6 +4,10 @@ import BackChevron from "redvest/components/BackChevron";
 
 import { createStackNavigator } from "@react-navigation/stack";
 import Invest from "redvest/screens/invest/Invest";
+import tickerInfo from "redvest/screens/info/tickerInfo";
+import typeInfo from "redvest/screens/info/typeInfo";
+import tfInfo from "redvest/screens/info/tfInfo";
+import slInfo from "redvest/screens/info/slInfo";
 
 const InvestStack = createStackNavigator();
 
@@ -18,6 +22,10 @@ function InvestStackNavigator() {
           title: "Invest Screen",
         }}
       />
+      <InvestStack.Screen name="tickerInfo" component={tickerInfo} />
+      <InvestStack.Screen name="typeInfo" component={typeInfo} />
+      <InvestStack.Screen name="tfInfo" component={tfInfo} />
+      <InvestStack.Screen name="slInfo" component={slInfo} />
     </InvestStack.Navigator>
   );
 }
